1fbe4e70f36901fc95c6cc34749eba7315c8efad
Slaychok <slachok@mail.ru>
2025-10-17 16:02:45 +0300

    +ListViewScreen

---
+++ lib/screens/listview_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:pr4/models/list_item.dart';
+
+class ListViewScreen extends StatefulWidget {
+  const ListViewScreen({super.key});
+
+  @override
+  State<ListViewScreen> createState() => _ListViewScreenState();
+}
+
+class _ListViewScreenState extends State<ListViewScreen> {
+  final List<ListItem> _tasks = [];
+  final _textController = TextEditingController();
+
+  void _addTask() {
+    final text = _textController.text.trim();
+    if (text.isEmpty) return;
+
+    setState(() {
+      _tasks.add(ListItem(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        title: text,
+      ));
+      _textController.clear();
+    });
+  }
+
+  void _removeTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Расходы - ListView')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: const InputDecoration(
+                      hintText: 'Введите расход',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: _addTask,
+                  child: const Text('Добавить'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: _tasks.isEmpty
+                ? const Center(child: Text('Нет расходов'))
+                : ListView(
+              children: [
+                for (int i = 0; i < _tasks.length; i++)
+                  ListTile(
+                    title: Text(_tasks[i].title),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => _removeTask(i),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
